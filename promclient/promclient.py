e5eabfdbadee36c218745e675acc9486a3b2354b
Erwin <34348385+ErwinRussel@users.noreply.github.com>
2022-06-05 12:32:54 -0700

    remove prom stuff and add pushing client

--- promclient/promclient.py
+++ promclient/promclient.py
@@ -1,49 +1,54 @@
-from prometheus_client import start_http_server, Summary, Gauge
-import random
 import time
 from os.path import exists
+import os
+import requests
+import socket
 
 file_exists = False
-sleep_time = 0
-overhead = 0
-target_frame_time = 0
-test_time = 0
+# sleep_time = 0
+# overhead = 0
+# target_frame_time = 0
+# frame_time = 0
 FILE = "./strangle.prom"
-
-g_sleep_time = Gauge('sleep_time', 'Sleep of the limiter in nanoseconds')
-g_overhead = Gauge('overhead', 'Overhead of the limiter in nanoseconds')
-g_target_frame_time = Gauge('target_frame_time', 'Target frame time of the limiter in nanoseconds')
-g_fps = Gauge('fps', 'Current frames per second of application')
-g_adjusted_sleep_time = Gauge('adjusted_sleep_time', 'Adjusted sleep of the limiter in nanoseconds')
-g_achievable_fps = Gauge('achievable_fps', 'Achievable frames per second of application')
-g_free_fps = Gauge('free_fps', 'Free frames per second of the limiter')
-g_target_fps = Gauge('target_fps', 'Frames per second set by strangle')
 
 # desired fps
 # int(1000000000 / target_frame_time)
 
-def process():
-    g_sleep_time.set(sleep_time)
-    g_overhead.set(overhead)
-    g_target_frame_time.set(target_frame_time)
-    g_adjusted_sleep_time.set(sleep_time - overhead)
+def process(sleep_time, overhead, target_frame_time, cur_frame_time):
+    push_dict = {}
+    push_dict['host_name'] = os.getenv('NODENAME')# todo: get_from_env
+    push_dict['container_name'] = socket.gethostname()
+
+    adjusted_sleep_time = sleep_time - overhead
+    push_dict['sleep_time'] = sleep_time
+    push_dict['overhead'] = overhead
+    push_dict['adjusted_sleep_time'] = adjusted_sleep_time
+    push_dict['target_frame_time'] = target_frame_time
 
     if(target_frame_time > 0):
-        g_target_fps.set(1000000000 / target_frame_time)
+        target_fps = 1000000000 / target_frame_time
+        push_dict['target_fps'] = target_fps
 
-    if(test_time > 0):
-        g_fps.set(1000000000 / test_time)
+    if(cur_frame_time > 0):
+        fps = 1000000000 / cur_frame_time
+        push_dict['fps'] = fps
 
-    if(test_time > 0 and sleep_time > 0):
-        g_achievable_fps.set(1000000000 / (test_time - sleep_time))
+    if(cur_frame_time > 0 and sleep_time > 0):
+        achievable_fps = 1000000000 / (cur_frame_time - sleep_time)
+        push_dict['achievable_fps'] = achievable_fps
 
-    if(overhead > 0):
-        g_free_fps.set((1000000000 / (test_time - sleep_time)) - (1000000000 / sleep_time))
+        if(overhead > 0):
+            free_fps = achievable_fps - (1000000000 / sleep_time)
+            push_dict['free_fps'] = free_fps
+
+    # todo: put this in a JSON and send to
+    try:
+        r = requests.post('http://0.0.0.0:8001/push_metric', json=push_dict)
+        print(f"Status Code: {r.status_code}") # , Response: {r.json()}
+    except:
+        print("Could not connect to port 8001")
 
 if __name__ == '__main__':
-    # Start up the server to expose the metrics.
-    start_http_server(8000)
-    # Generate some requests.
     file_exists = exists(FILE)
     while True:
         # read from file
@@ -55,7 +60,7 @@ if __name__ == '__main__':
                 sleep_time = int(data[0])
                 overhead = int(data[1])
                 target_frame_time = int(data[2])
-                test_time = int(data[3])
+                cur_frame_time = int(data[3])
 
-        process()
+                process(sleep_time, overhead, target_frame_time, cur_frame_time)
         time.sleep(5)
